6bed67bc6e2216b39fd878d7c4fd896ba7480206
robin-raq <robin.raq@gmail.com>
2019-11-18 17:38:54 -0500

    Done.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function moveDodgerLeft() {
     let leftNumbers = dodger.style.left.replace("px", "");
     let left = parseInt(leftNumbers, 10);
    
-    if (left > 0) {
+    if (left > 0 ) {
       dodger.style.left = `${left - 1}px`;
     }
   }
@@ -16,7 +16,7 @@ function moveDodgerLeft() {
     let leftNumbers = dodger.style.left.replace("px", "");
     let left = parseInt(leftNumbers, 10);
    
-    if (left > 0) {
+    if (left > 0 && left <360) {
       dodger.style.left = `${left + 1}px`;
     }
   }
